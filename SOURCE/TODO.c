5b8a81880b0a7c45cd92de8a6fa30189c0ebee37
Ali Jangda <ali.jangda@gmail.com>
2010-11-10 16:48:49 -0500

    Moved struct/ folder and all files into kern/struct. Updated Makefile to support. Added a TODO file.

---
+++ SOURCE/TODO.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	// TODO
+	// STRUCTS
+	io_buffer.c
+	timeout_queue.c
+
+	// Kernel
+	scheduler.c // queue.c, priority_queue.c
+
+	api.c		// all kernel
+
+	// Processes	// api.c for all
+	user_a.c		
+	user_b.c
+	user_C.c
+	null.c
+	wall_clock.c
+	cci.c
+	i_timer.c	
+	i_crt.c
+	i_kb.c			
+
+	// Helpers
+	kb_helper.c   	// io_buffer.c
+	crt_help.c	// io_buffer.c	
