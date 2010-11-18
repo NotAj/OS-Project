42036632cb366ea85342423ab0f6055dcad845b1
NotAj <ali.jangda@gmail.com>
2010-11-18 18:45:13 -0500

    Updated templates, added base scheduler file.

--- SOURCE/inc/k_scheduler.h
+++ SOURCE/inc/k_scheduler.h
@@ -1,39 +1,40 @@
-#ifndef K_QUEUE_H
-#define k_QUEUE_H
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
-#include "k_pcb.h"
+#include "pcb.h"
+#include "queue.h"
+#include "priority_queue.h"
 
 /****************************************************************
- Queue Data structure
+ Scheduler
  ----------------------------------------------------------------
- This structure is used for the allQ which contains all processes 
- in the RTX.
- It is also used inside the Priority Queue structure, which is an
- array of these queues. The readyQ and blockedQ are examples of 
- these. 
+ The scheduler consists of the pcb, queue and priority_queue data
+ structures. It serves to consolidate all the includes so that the
+ api functions only need to include the scheduler. IT MIGHT BECOME
+ A STRUCT WITH A GLOBAL INSTANCE THAT HAS BOTH SCHEDULING QUEUES.
 
 *****************************************************************/
 
 /*************************
 * Type Declarations
 *************************/
-typedef struct k_queue_struct *k_queue_ptr;
+/*typedef struct k_queue_struct *k_queue_ptr;
 
 typedef struct k_queue_struct
 {	
 	k_PCB_ptr head;
 	k_PCB_ptr tail;
 } k_queue;
-
+*/
 /*************************
 * Function Declarations
 *************************/
-int k_queue_is_empty (k_queue_ptr Q);
+/*int k_queue_is_empty (k_queue_ptr Q);
 
 void k_queue_enqueue (k_PCB_ptr process, int all_Q, k_queue_ptr Q);
 
 k_PCB_ptr k_queue_dequeue (k_queue_ptr Q);
 
 k_PCB_ptr k_queue_remove(int PID, k_queue_ptr Q);
-
+*/
 #endif
---
+++ SOURCE/kern/k_scheduler.c
@@ -0,0 +1,13 @@
+#include "k_scheduler.h"
+
+/****************************************************************
+ Scheduler
+ ----------------------------------------------------------------
+ The scheduler consists of the pcb, queue and priority_queue data
+ structures. It serves to consolidate all the includes so that the
+ api functions only need to include the scheduler. IT MIGHT BECOME
+ A STRUCT WITH A GLOBAL INSTANCE THAT HAS BOTH SCHEDULING QUEUES.
+
+*****************************************************************/
+
+
