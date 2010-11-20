f926b92b1fc13b2773afc0845db00e66113a2e99
Ali Jangda <ali.jangda@gmail.com>
2010-11-19 22:59:06 -0500

    Added k_ prefix to defines.h and globals.h, to match all other files

---
+++ SOURCE/inc/k_defines.h
@@ -0,0 +1,56 @@
+#ifndef K_DEFINES_H
+#define K_DEFINES_H
+
+/***************************************
+ Message Envelope Defines
+****************************************/
+#define MSG_ENV_SIZE 256 	// Size of the Message Envelope data field (bytes)
+#define MSG_ENV_NUM 50 		// Number of Message Envelopes
+
+/***************************************
+ Process Defines
+****************************************/
+#define PROCESS_NUM 11 		// Number of processes in the RTX 
+#define STACK_SIZE 1024		// Size of the stack for each process (in Bytes)
+#define PRIORITY_NUM 4 		// Number of priority levels in the OS
+
+/***************************************
+ IO Helper Defines
+****************************************/
+#define BUFFER_SIZE 512		// Size of the shared IO buffers
+
+/***************************************
+ Tracebuffer Defines
+****************************************/
+#define TRACEBUFFER_SIZE 16 // Number of traces
+
+
+/***************************************
+ PID Defines
+****************************************/
+#define PID_NULL 0 			// PID of NULL process
+#define PID_WALL_CLOCK 10 	// PID of Wall Clock process
+#define PID_CCI 11 			// PID of CCI Process
+#define PID_I_TIMER 12 		// PID of Timer iprocess
+#define PID_I_CRT 13 		// PID of CRT iprocess
+#define PID_I_KB 14 		// PID of KB iprocess
+
+/***************************************
+ Status Defines
+****************************************/
+#define STATUS_EXECUTING 0				// Status when executing
+#define STATUS_READY 1					// Status when ready
+#define STATUS_BLOCKED_ON_RECEIVE 2		// Status when blocked on receive
+#define STATUS_BLOCKED_ON_RESOURCE 3	// Status when blocked on resource
+#define STATUS_INTERRUPTED 4			// Status when interrupted
+#define STATUS_IPROCESS 5				// Status if iprocess
+
+/***************************************
+ Message Type Defines
+****************************************/
+#define MSG_TYPE_OUTPUT_REQUEST 0	// Used by send_console_chars()
+#define MSG_TYPE_INPUT_REQUEST 1	// Used by get_console_chars()
+#define MSG_TYPE_CONSOLE_INPUT 2	// Used by KB iprocess
+#define MSG_TYPE_WAKEUP_CODE 3		// Used by Timer iprocess
+
+#endif
