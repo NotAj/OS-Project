237b3bb2cff7fa3c2e0df0d32f840b41b40f9eea
Ali Jangda <ali.jangda@gmail.com>
2010-11-22 18:15:51 -0500

    Added support for printing the allQ to the test_queue functions. Updated test_priority_queue and all other places it's called

--- SOURCE/test/test_queue.c
+++ SOURCE/test/test_queue.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void test_queue_print(k_queue_ptr Q)
+void test_queue_print(int is_allQ, k_queue_ptr Q)
 {
 	int i = 0;
 	k_PCB_ptr curr_PCB = NULL;
@@ -12,14 +12,26 @@ void test_queue_print(k_queue_ptr Q)
 		return;
 	}
 	printf("**QUEUE**\n");
-	curr_PCB = Q->head;	
-	printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_queue_next);
-	i++;	
-	while (curr_PCB->k_queue_next != NULL)
+	curr_PCB = Q->head;
+	if (is_allQ)
 	{
-		curr_PCB = curr_PCB->k_queue_next;
-		printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_queue_next);
-		i++;
+		printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_all_queue_next);
+		while (curr_PCB->k_all_queue_next != NULL)
+		{
+			curr_PCB = curr_PCB->k_all_queue_next;
+			printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_all_queue_next);
+			i++;
+		}
 	}
+	else
+	{
+		printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_queue_next);
+		while (curr_PCB->k_queue_next != NULL)
+		{
+			curr_PCB = curr_PCB->k_queue_next;
+			printf("Item %d = %d || Next = %p \n", i, curr_PCB->p_pid, curr_PCB->k_queue_next);
+			i++;
+		}
+	}	
 	printf("\n");
 }
