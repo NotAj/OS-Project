eb218f805cb1fdeb372846b9000cff4b0a07a3e4
Ali Jangda <ali.jangda@gmail.com>
2010-11-21 23:05:39 -0500

    Added initialization of globals where possible (excluding all structs)

--- SOURCE/inc/k_globals.h
+++ SOURCE/inc/k_globals.h
@@ -8,24 +8,26 @@
 #include "k_priority_queue.h"
 #include "k_message_queue.h"
 #include "k_tracebuffer.h"
+#include "k_itable.c"
+#include "k_init_struct.h"
 
 /***************************************
  Global Variables
 ****************************************/
-k_PCB_ptr k_current_process;		// Points to PCB of currently executing process
-k_PCB_ptr k_interrupted_process;	// Points to PCB of interrupted process
-int k_atomic_flag;					// Flag controlling calls to atomic
-//#ifdef K_QUEUE_H
+k_PCB_ptr k_current_process= NULL;	// Points to PCB of currently executing process
+k_PCB_ptr k_interrupted_process = NULL;	// Points to PCB of interrupted process
+int k_atomic_flag = 0;				// Flag controlling calls to atomic
 k_queue_ptr k_allQ;					// Queue contains all process in the RTX
-k_priority_queue_ptr k_readyQ;		// PQ containing all ready process
-k_priority_queue_ptr k_blockedQ;	// PQ containing all blocked_on_allocate processes
-k_message_queue k_freeQ;			// Queue containing all free message envelopes
-int k_clock_h;						// Wall Clock time in hours
-int k_clock_m;						// Wall Clock time in minutes
-int k_clock_s;						// Wall Clock time in seconds
-int k_clock_tick;					// System time in clock ticks (100ms per tick)
-int k_display_clock;				// Flag controlling display of wall clock
-k_tracebuffer_ptr k_send_TB;		// Sent message TB array (16 last sends)
-k_tracebuffer_ptr k_receive_TB;   // Receive message TB array (16 last receives)
+k_priority_queue_ptr k_readyPQ; 		// PQ containing all ready process
+k_priority_queue_ptr k_blockedPQ; 	// PQ containing all blocked_on_allocate processes
+k_message_queue_ptr k_freeMQ; 		// Queue containing all free message envelopes
+int k_clock_h = 0;					// Wall Clock time in hours
+int k_clock_m = 0;					// Wall Clock time in minutes
+int k_clock_s = 0;					// Wall Clock time in seconds
+int k_clock_tick = 0;				// System time in clock ticks (100ms per tick)
+int k_display_clock = 0;			// Flag controlling display of wall clock
+k_tracebuffer_ptr k_sendTB;			// Sent message TB array (16 last sends)
+k_tracebuffer_ptr k_receiveTB;		// Receive message TB array (16 last receives)
+k_itable_ptr itable;				// Itable used to initialize all processes
 
 #endif
