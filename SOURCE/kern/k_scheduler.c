266fd41a3832f9b0f1148920da47483507881f52
Ali Jangda <ali.jangda@gmail.com>
2010-11-22 14:45:23 -0500

    Forgot to close off if statement in change to context_switch, fixed

--- SOURCE/kern/k_scheduler.c
+++ SOURCE/kern/k_scheduler.c
@@ -60,10 +60,11 @@ void k_process_switch ( )
 void k_context_switch (k_PCB_ptr prev_process, k_PCB_ptr next_process)
 {	
 	extern k_PCB_ptr k_current_process;
-
-	if (prev_process == NULL || next_process == NULL)
-		//k_terminate(); // Context switch should always be given valid parameters
 	
+	if (prev_process == NULL || next_process == NULL)
+	{	
+		//k_terminate(); // Context switch should always be given valid parameters
+	}
 	// Setting the current_process global here since context_switch won't work the first time unless current_process is set correctly, and don't want to forget it outside
 	// REMEMBER this change for signal handler
 	k_current_process = next_process;
