a9ddcbdfb96c7ad56539ba9d26ec747a8a99e16a
Ali Jangda <ali.jangda@gmail.com>
2010-11-23 12:12:11 -0500

    Added files for user visible API functions. Most primitives empty at the moment

---
+++ SOURCE/kern/k_api.c
@@ -0,0 +1,68 @@
+#include "k_api.h"
+
+/****************************************************************
+ User API
+ ----------------------------------------------------------------
+ This file has all the user visible API function available to 
+ user processes. These primitives disable interrupts, then call the 
+ kernel version of these primitives. They reenable interrupts before
+ returning.  
+
+*****************************************************************/
+/*************************
+* Scheduler primitives 
+*************************/
+int release_processor()
+{
+	atomic(on);
+	k_release_processor();
+	atomic(off);
+	
+}
+
+int request_process_status(MsgEnv *msg_env_ptr)
+{}
+
+int terminate()
+{}
+
+int change_priority(int new_priority, int target_process)
+{
+	atomic(on);
+	k_change_priority(int new_priority, int target_process);
+	atomic(off);
+}
+
+/*************************
+* IPC primitives 
+*************************/
+int send_message(int dest_process_id, MsgEnv *msg_envelope)
+{}
+
+MsgEnv *receive_message()
+{}
+
+MsgEnv *request_msg_env()
+{}
+
+int release_msg_env(MsgEnv *msg_env_ptr)
+{}
+
+int get_trace_buffers(MsgEnv *message_envelope)
+{}
+
+/*************************
+* Timing primitives 
+*************************/
+int request_delay(int time_delay, int wakeup_code, MsgEnv *message_envelope)
+{}
+
+/*************************
+* IO primitives 
+*************************/
+int send_console_chars(MsgEnv *message_envelope)
+{}
+
+int get_console_chars(MsgEnv *message_envelope)
+{}
+
