ce597a4ab53842440e4b0fab0a0292b297a5e649
alroyal <alroyal1@gmail.com>
2010-11-24 23:19:34 -0600

    minor tweaks

--- SOURCE/test/main_kbd.c
+++ SOURCE/test/main_kbd.c
@@ -27,9 +27,6 @@ int main(){
 	char c;
 	int i;
 	
-	printf("\n\nRTX_pid = %d\n",RTX_pid);
-	printf("fid = %d\n",fid);
-	
 	/************Forking into Keyboard Helper************/
 	int newPID = fork();
 	if(newPID == 0)				//Check that fork was successful
@@ -37,38 +34,32 @@ int main(){
 		i = execl("./helpers/kbd_helper", "kbd_helper", kbd_info1, kbd_info2, (char *)0);
 		printf("execl failed if here.  execl = %d\n",i);
 	}
-	printf("child PID = %d\n",newPID);
 
 	/************Mapping memory to the file************/
-
 	mmap_ptr = mmap((caddr_t) 0,			// Memory Location, 0 lets OS choose
 			BUFFER_SIZE,			// How many bytes to mmap
 			PROT_READ | PROT_WRITE, 	// Read and write permissions
 			MAP_SHARED,    			// Accessible by another process
 			fid,           			// Which file is associated with mmap
 			(off_t) 0);			// Offset in page frame
-
 	assert(mmap_ptr != MAP_FAILED);
 	
 	input_buf = (k_io_buffer_ptr) mmap_ptr;		//creating pointer to the sharedmem
 
 	/************Testing helper process************/
-	printf("input_buf->length = %d\n",input_buf->length);
 	while(1)
 	{
-//		printf("I have a feeling i just created infinite printf's");
 		if(input_buf->wait_flag == 1)
 		{
-			printf("User input:  ");
+			printf("User input :  ");
 			for(i=0;i<input_buf->length;i++)
 			{
-			//	c = input_buf->bufdata[i];
-				printf("%c",input_buf->bufdata[i]);
+				c = input_buf->bufdata[i];
+				printf("%c",c);
 			}
 			printf("\n");
 			input_buf->wait_flag = 0;
 			input_buf->length = 0;
-			
 		}
 	}
 
