5209a75e85e06eb68481fe142a2e38a223662eca
Ali Jangda <ali.jangda@gmail.com>
2010-11-28 00:46:49 -0500

    Changed number of msg envelopes to 20

--- SOURCE/inc/k_defines.h
+++ SOURCE/inc/k_defines.h
@@ -5,7 +5,7 @@
  Message Envelope Defines
 ****************************************/
 #define MSG_ENV_SIZE 512 	// Size of the Message Envelope data field (bytes)
-#define MSG_ENV_NUM 8		// Number of Message Envelopes
+#define MSG_ENV_NUM 20		// Number of Message Envelopes
 
 /***************************************
  Process Defines
